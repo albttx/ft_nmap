85f25af25f23be5b0140e5dfdff222d56cfb4a4e
Albert Le Batteux <ale-batt@student.42.fr>
2017-08-06 22:04:40 +0200

    [+] working on

--- sources/scan/scan_udp.c
+++ sources/scan/scan_udp.c
@@ -6,14 +6,14 @@
 /*   By: ale-batt <ale-batt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/02 17:22:03 by ale-batt          #+#    #+#             */
-/*   Updated: 2017/08/02 17:30:33 by ale-batt         ###   ########.fr       */
+/*   Updated: 2017/08/05 11:57:14 by ale-batt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nmap.h"
 
 /*
- *  -= ACK SCAN =-
+ *  -= UDP SCAN =-
  *
  *  Send ACK
  *  Wait for :
@@ -34,5 +34,27 @@ int		udp_set(enum e_tcp_type types)
 
 void	scan_udp(char *ipv4name)
 {
+	int					i;
+	int					ret;
+	int					sock;
+	struct sockaddr_in	sin;
 
+	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sock <= 0)
+	{
+		perror("UDP socket");
+		exit(EXIT_FAILURE);
+	}
+	ft_bzero(&sin, sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = inet_addr(ipv4name);
+	i = g_env.port[0];
+	while (i < g_env.port[1])
+	{
+		sin.sin_port = htons(i);
+		ret = sendto(sock, "42", 2, 0, (struct sockaddr *)&sin, sizeof(sin));
+		if (ret <= 0)
+			perror("udp sendto()");
+		i++;
+	}
 }
